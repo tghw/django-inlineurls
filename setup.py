44426334653e03cb3863267020ddaf2060cb7f91
Tyler G. Hicks-Wright <tyler@hicks-wright.net>
2010-09-23 23:10:18 -0400

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='inlineurls',
+      version='1.0',
+      description='Decorators to make writing Django views easier',
+      author='Tyler G. Hicks-Wright',
+      author_email='tyler@hicks-wright.net',
+      url='http://bitbucket.org/tghw/inlineurls',
+      packages=['inlineurls',],
+ )
